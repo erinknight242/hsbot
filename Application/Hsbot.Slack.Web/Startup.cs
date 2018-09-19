31d2114f50107511ddf01a465fbb76dbbe8586ab
Dean Goodman <dean.goodman@headspring.com>
2018-09-18 21:57:17 -0500

    Remove extra boilerplate

--- Application/Hsbot.Slack.Web/Startup.cs
+++ Application/Hsbot.Slack.Web/Startup.cs
@@ -1,7 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -11,13 +7,10 @@ namespace Hsbot.Slack.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
