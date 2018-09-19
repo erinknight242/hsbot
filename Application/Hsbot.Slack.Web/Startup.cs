3a7e3aad0ab105c8885080cc6668d35afa8171f5
Dean Goodman <dean.goodman@headspring.com>
2018-09-19 08:15:43 -0500

    Add comments briefly explaining the hosting setup

--- Application/Hsbot.Slack.Web/Startup.cs
+++ Application/Hsbot.Slack.Web/Startup.cs
@@ -21,20 +21,27 @@ namespace Hsbot.Slack.Web
       public void ConfigureServices(IServiceCollection services)
       {
         services.AddHsbot(new HsbotConfig {SlackApiKey = _config["slack:apiKey"]});
+
+        //This registration is what will actually run hsbot as a background
+        //process within the website.  We'll need an external keep-alive to
+        //make sure the site doesn't shut down when no HTTP requests are coming in.
         services.AddSingleton<IHostedService, HsbotHostedService>();
       }
 
       public void Configure(IApplicationBuilder app, IHostingEnvironment env)
       {
-          if (env.IsDevelopment())
-          {
-              app.UseDeveloperExceptionPage();
-          }
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
 
-          app.Run(async (context) =>
-          {
-              await context.Response.WriteAsync("Beep boop bop");
-          });
+        //We're not actually serving up any web content at present.
+        //This website is only acting as a host for the hsbot service,
+        //so no need to wire up anything other than a static reply.
+        app.Run(async (context) =>
+        {
+            await context.Response.WriteAsync("Beep boop bop");
+        });
       }
     }
 }
